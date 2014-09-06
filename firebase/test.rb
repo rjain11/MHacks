b744e25a2483c73c89dc9a62e004ff4057493ce4
rishabhjain <rishabhjain2795@gmail.com>
2014-09-06 02:39:22 -0400

    reading

--- firebase/test.rb
+++ firebase/test.rb
@@ -2,5 +2,6 @@ require 'firebase'
 
 base_uri = 'https://mhacks2014.firebaseio.com/'
 firebase = Firebase::Client.new(base_uri)
-response = firebase.update("Record",{:videoid => 'https://youtube.net/assets', :seconds => 10 })
+response = firebase.get("mhacks2014",{:videoid => 'https://youtube.net/assets', :seconds => 10 })
+puts(response)
 response.success? # => true
